87d01150fe09e5be9af22310e6380ddb3c7571db
Yvain.Yan <yanyongheng666666@gmail.com>
2026-09-08 22:02:05 +0800

    generate select sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 using namespace std;
+
+//生成一个选择排序
+void selectSort(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+
 int main()
 {
     std::cout<<"hello git!"<<std::endl;
